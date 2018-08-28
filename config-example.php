16805942c61bf667f72036017ea897ebc9083273
blue <937069176@qq.com>
2018-08-28 15:05:11 +0800

    add 示例配置文件

---
+++ config-example.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: blue
+ * Date: 2018/8/28
+ * Time: 14:31
+ */
+
+return [
+    'baidu_speech'=>[
+        'appId'  =>   '',
+        'appKey' =>   '',
+        'appSecret' =>   ''
+    ],
+    'tuling_robot'=>[
+        'appkey'=>'',
+        'apihost'=>''
+    ]
+];
---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: blue
+ * Date: 2018/8/28
+ * Time: 14:31
+ */
+
+return [
+    'baidu_speech'=>[
+        'appId'  =>   '11589592',
+        'appKey' =>   '4v3n8TkRSlmm6miKxj36N8Sp',
+        'appSecret' =>   'RDvnN5GwLCXCvRtGIRnqyxB0jEDxAq7E'
+    ],
+    'tuling_robot'=>[
+        'appkey'=>'e19a70fbd8cd41698d5e1f564be95631',
+        'apihost'=>'http://www.tuling123.com/openapi/api'
+    ]
+];
